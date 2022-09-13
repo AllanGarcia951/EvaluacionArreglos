d1f649943fb26bd9d558c084f565338fc5ac23f6
Allan Garcia <111915221+AllanGarcia951@users.noreply.github.com>
2022-09-12 23:10:24 -0600

    Add files via upload

---
+++ AllanG/EvaluacionArreglos/src/dao/DPersona.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import modelos.Persona;
+
+/**
+ *
+ * @author labc205
+ */
+public class DPersona {
+    private ArrayList<Persona> listPersona=new ArrayList<>();
+
+    public DPersona() {
+    }
+
+    public DPersona(ArrayList<Persona> listPersona) {
+        this.listPersona = listPersona;
+    }
+
+    public ArrayList<Persona> getListPersona() {
+        return listPersona;
+    }
+
+    public void setListPersona(ArrayList<Persona> listPersona) {
+        this.listPersona = listPersona;
+    }
+    
+    /**
+     * Permite agregar un nuevo registro a mi lista
+     * @param id
+     * @param nom
+     * @param ape
+     * @param email
+     * @param sexo
+     * @return 
+     */
+    public int agregarPersona(int id,
+            String nom, String ape, 
+            String email){
+        int b =0;
+        Persona pers = new Persona(id, nom, ape, email);
+        listPersona.add(pers);
+        b = 1;
+        return b;
+    }
+    /**
+     * Devuelve una tabla con los registros 
+     * almacenados en el listado de personas
+     * @return 
+     */
+    public DefaultTableModel getListPers(){
+        DefaultTableModel dtm = new DefaultTableModel();
+        String titulo[] ={"ID", "NOMBRE", "PRECIO",
+            "EXISTENCIA",};
+        //Asigna los encabezados a mi tabla
+        dtm.setColumnIdentifiers(titulo);
+        
+        //Estoy usando for each
+        for (Persona per: listPersona){
+            String reg[] = new String[4];
+            reg[0] = ""+per.getId();
+            reg[1] = per.getNombre();
+            reg[2] = per.getApellidos();
+            reg[3] = per.getEmail();
+         
+            
+            dtm.addRow(reg);
+        }
+        return dtm;
+    }
+  
+}
---
+++ AllanG/EvaluacionArreglos/src/modelos/Persona.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author labc205
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+ 
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellidos, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+   
+    }
+
+
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+
+    @Override
+    public String toString() {
+        return '}' +  "Persona{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos;
+    }
+    
+    
+}
